4caf9e11056a40aa7255533162d24778c53d2335
Artur Arantes T <artur.a.tiraboschi@gmail.com>
2021-10-13 19:36:45 -0300

    feat(#1nv7142): add flyway depedency
    Signed-off-by: Artur Arantes T <artur.a.tiraboschi@gmail.com>

--- src/main/java/br/com/artur/offnance/config/Constants.java
+++ src/main/java/br/com/artur/offnance/config/Constants.java
@@ -3,9 +3,11 @@ package br.com.artur.offnance.config;
 /**
  * Application constants.
  */
+
 public final class Constants {
 
-    public static final String SYSTEM = "system";
+  public static final String SYSTEM = "system";
 
-    private Constants() {}
+  private Constants() {
+  }
 }
