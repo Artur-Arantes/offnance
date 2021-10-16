b1ee6379e25dfdedd1322c9950fc928236fc29e6
Henrique Arantes Tiraboschi <henrique_arantes_338@hotmail.com>
2021-10-16 10:11:45 -0300

    Revert "feat(#1nv7164): create user entity"
    This reverts commit 978a1491e958441ecc4fc485e1f5c499f5e49369.

--- src/test/java/br/com/artur/offnance/OffnanceApplicationTests.java
+++ src/test/java/br/com/artur/offnance/OffnanceApplicationTests.java
@@ -3,7 +3,6 @@ package br.com.artur.offnance;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
 @SpringBootTest
 class OffnanceApplicationTests {
 
