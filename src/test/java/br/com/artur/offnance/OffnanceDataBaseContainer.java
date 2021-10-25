6e14e2f631186596012c57c0ae4929d6a5cb69f5
Henrique Arantes Tiraboschi <henrique_arantes_338@hotmail.com>
2021-10-25 13:10:33 -0300

    feat(#1pj9a4z): update circle-ci

--- src/test/java/br/com/artur/offnance/OffnanceDataBaseContainer.java
+++ src/test/java/br/com/artur/offnance/OffnanceDataBaseContainer.java
@@ -4,32 +4,46 @@ import org.testcontainers.containers.MySQLContainer;
 
 public class OffnanceDataBaseContainer extends MySQLContainer<OffnanceDataBaseContainer> {
 
-  public static final String MYSQL_VERSION = "mysql:5.7";
-  public static final String APP_NAME = "offnance";
+    public static final String MYSQL_VERSION = "mysql:5.7";
+    public static final String APP_NAME = "offnance";
 
-  public static OffnanceDataBaseContainer container;
+    private boolean isActive;
 
-  public static OffnanceDataBaseContainer getInstance() {
-    if (container == null) {
-      container = new OffnanceDataBaseContainer();
+    public static OffnanceDataBaseContainer container;
+
+    public static OffnanceDataBaseContainer getInstance() {
+        return getInstance(true);
     }
-    return container;
-  }
 
-  public OffnanceDataBaseContainer() {
-    super(MYSQL_VERSION);
-    this.withUsername(APP_NAME).withDatabaseName(APP_NAME).withPassword(APP_NAME)
-        .withReuse(false);
-  }
-  @Override
-  public void start() {
-    super.start();
-    System.setProperty("DB_URL", container.getJdbcUrl());
-    System.setProperty("DB_USR", container.getUsername());
-    System.setProperty("DB_PASS", container.getPassword());
-  }
+    public static OffnanceDataBaseContainer getInstance(final boolean isActive) {
+        if (container == null) {
+            container = new OffnanceDataBaseContainer(isActive);
+        }
+        return container;
+    }
 
-  @Override
-  public void stop() {
-  }
+    public OffnanceDataBaseContainer(final boolean isActive) {
+        super(MYSQL_VERSION);
+        this.withUsername(APP_NAME).withDatabaseName(APP_NAME).withPassword(APP_NAME)
+                .isActive(isActive).withReuse(false);
+    }
+
+    private OffnanceDataBaseContainer isActive(boolean isActive) {
+        this.isActive = isActive;
+        return this;
+    }
+
+    @Override
+    public void start() {
+        if (this.isActive) {
+            super.start();
+            System.setProperty("DB_URL", container.getJdbcUrl());
+            System.setProperty("DB_USR", container.getUsername());
+            System.setProperty("DB_PASS", container.getPassword());
+        }
+    }
+
+    @Override
+    public void stop() {
+    }
 }
--- src/test/java/br/com/artur/offnance/controller/LoginControllerIntegrationTest.java
+++ src/test/java/br/com/artur/offnance/controller/LoginControllerIntegrationTest.java
@@ -3,6 +3,7 @@ package br.com.artur.offnance.controller;
 import br.com.artur.offnance.OffnanceDataBaseContainer;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -14,54 +15,57 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Testcontainers
+@Testcontainers(disabledWithoutDocker = true)
 @DisplayName("Teste para integração de login")
 public class LoginControllerIntegrationTest {
 
-  @LocalServerPort
-  private Integer port;
+    @LocalServerPort
+    private Integer port;
 
-  @Container
-  public static OffnanceDataBaseContainer container = OffnanceDataBaseContainer.getInstance();
+    public static final String ENV_DISABLE_TEST_CONTAIENRS = "DISABLE_TEST_CONTAIENRS";
 
-  static {
-    container.start();
-  }
+    @Container
+    public static OffnanceDataBaseContainer container = OffnanceDataBaseContainer
+            .getInstance(StringUtils.isBlank(System.getenv(ENV_DISABLE_TEST_CONTAIENRS)));
 
-  @BeforeEach
-  void setUp() {
-    RestAssured.port = port;
-    RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-  }
-
-  @Nested
-  @DisplayName("testes para efetuar o login")
-  class LoginTest {
-
-    @Test
-    @DisplayName("teste de login com sucesso")
-    void testLoginSuccess() {
-      RestAssured.given().contentType(ContentType.URLENC)
-          .formParam("username", "admin")
-          .formParam("password", "admin")
-          .when()
-          .post("/api/login")
-          .then()
-          .statusCode(HttpStatus.OK.value());
+    static {
+        container.start();
     }
 
-    @Test
-    @DisplayName("teste de login com falha")
-    void testLoginFailure() {
-      RestAssured.given().contentType(ContentType.URLENC)
-          .formParam("username", "admin")
-          .formParam("password", "x")
-          .when()
-          .post("/api/login")
-          .then()
-          .statusCode(HttpStatus.UNAUTHORIZED.value());
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
     }
 
-  }
+    @Nested
+    @DisplayName("testes para efetuar o login")
+    class LoginTest {
+
+        @Test
+        @DisplayName("teste de login com sucesso")
+        void testLoginSuccess() {
+            RestAssured.given().contentType(ContentType.URLENC)
+                    .formParam("username", "admin")
+                    .formParam("password", "admin")
+                    .when()
+                    .post("/api/login")
+                    .then()
+                    .statusCode(HttpStatus.OK.value());
+        }
+
+        @Test
+        @DisplayName("teste de login com falha")
+        void testLoginFailure() {
+            RestAssured.given().contentType(ContentType.URLENC)
+                    .formParam("username", "admin")
+                    .formParam("password", "x")
+                    .when()
+                    .post("/api/login")
+                    .then()
+                    .statusCode(HttpStatus.UNAUTHORIZED.value());
+        }
+
+    }
 
 }
