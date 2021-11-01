a4af8a35882325628116c389f720f48d4e7a624a
Henrique Arantes Tiraboschi <henrique_arantes_338@hotmail.com>
2021-11-01 20:12:10 -0300

    feat(#1q3atfw): configure herokuapp
    Signed-off-by: Henrique Arantes Tiraboschi <henrique_arantes_338@hotmail.com>

---
+++ readme.md
@@ -0,0 +1,3 @@
+# OFFNANCE
+[![Coverage Status](https://coveralls.io/repos/github/Artur-Arantes/offnance/badge.svg?branch=main)](https://coveralls.io/github/Artur-Arantes/offnance?branch=main)
+[![CircleCI](https://circleci.com/gh/Artur-Arantes/offnance/tree/main.svg?style=svg)](https://circleci.com/gh/Artur-Arantes/offnance/tree/main)
--- src/main/resources/db/migration/V202110171201__create_initial_data.sql
+++ src/main/resources/db/migration/V202110171201__create_initial_data.sql
@@ -1,6 +1,10 @@
-INSERT INTO permissao(nom_per) VALUES('USER');
-INSERT INTO permissao(nom_per) VALUES('ADMIN');
-INSERT INTO pessoa(nom_pes,ver_pes,cre_at_pes,upd_at_pes) VALUES('artur',1,now(), now());
-INSERT INTO usuario(sen_usu,log_usu,id_pes,acc_non_loc_usu,acc_non_exp_usu ,cre_non_exp_usu,ena_usu) VALUES( "$2a$04$UIeR96XNyDwQHJE4G2DWquYMZmopzZ0z5o5f1bc2KA.YgFhGU7/aW","admin",1,1,1,1,1);
+ALTER TABLE permissao AUTO_INCREMENT = 0;
+ALTER TABLE pessoa AUTO_INCREMENT = 0;
+ALTER TABLE usuario AUTO_INCREMENT = 0;
+ALTER TABLE usuario_permissao AUTO_INCREMENT = 0;
+INSERT INTO permissao(nom_per, id_per) VALUES('USER',1);
+INSERT INTO permissao(nom_per, id_per) VALUES('ADMIN',2);
+INSERT INTO pessoa(nom_pes,ver_pes,cre_at_pes,upd_at_pes, id_pes) VALUES('artur',1,now(), now(),1);
+INSERT INTO usuario(sen_usu,log_usu,id_pes,acc_non_loc_usu,acc_non_exp_usu ,cre_non_exp_usu,ena_usu, id_usu) VALUES( "$2a$04$UIeR96XNyDwQHJE4G2DWquYMZmopzZ0z5o5f1bc2KA.YgFhGU7/aW","admin",1,1,1,1,1,1);
 INSERT INTO usuario_permissao(id_per, id_usu) VALUES(1,1);
 INSERT INTO usuario_permissao(id_per,id_usu) VALUES(2,1);
