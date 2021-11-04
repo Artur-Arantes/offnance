d23958576a04022370a137ac3c9073f1886dc283
Release Script <builds@offnance-circle-ci.com>
2021-11-04 06:48:36 -0300

    feat(#1qer2tq): add cors configuration
    Signed-off-by: Release Script <builds@offnance-circle-ci.com>

--- src/main/java/br/com/artur/offnance/config/ContextConfiguration.java
+++ src/main/java/br/com/artur/offnance/config/ContextConfiguration.java
@@ -8,8 +8,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class ContextConfiguration {
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/br/com/artur/offnance/config/SecurityConfiguration.java
+++ src/main/java/br/com/artur/offnance/config/SecurityConfiguration.java
@@ -20,65 +20,87 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @DependsOn("passwordEncoder")
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-  @Autowired
-  private ConfigProperties configProperties;
+    @Autowired
+    private ConfigProperties configProperties;
 
-  @Autowired
-  private CustomUserDetailsServiceImp customUserDetailsServiceImp;
+    @Autowired
+    private CustomUserDetailsServiceImp customUserDetailsServiceImp;
 
-  @Autowired
-  private EntryPointAuthenticationRest entryPointAuthenticationRest;
+    @Autowired
+    private EntryPointAuthenticationRest entryPointAuthenticationRest;
 
-  @Autowired
-  private LogoutSuccessful logoutSuccessful;
+    @Autowired
+    private LogoutSuccessful logoutSuccessful;
 
-  @Autowired
-  private PasswordEncoder passwordEncoder;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-  @Autowired
-  private SuccessfulAuthenticationHandler successfulAuthenticationHandler;
+    @Autowired
+    private SuccessfulAuthenticationHandler successfulAuthenticationHandler;
 
-  @Autowired
-  private FailedAuthenticationHandler failedAuthenticationHandler;
+    @Autowired
+    private FailedAuthenticationHandler failedAuthenticationHandler;
 
-  @Bean
-  public AuthenticationTokenFilter jwtAuthenticationTokenFilter() throws Exception {
-    return new AuthenticationTokenFilter();
-  }
+    @Bean
+    public AuthenticationTokenFilter jwtAuthenticationTokenFilter() throws Exception {
+        return new AuthenticationTokenFilter();
+    }
 
-  @Bean
-  @Override
-  public AuthenticationManager authenticationManagerBean() throws Exception {
-    return super.authenticationManagerBean();
-  }
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
-  @Autowired
-  public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder)
-      throws Exception {
-    authenticationManagerBuilder.userDetailsService(customUserDetailsServiceImp)
-        .passwordEncoder(passwordEncoder);
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder)
+            throws Exception {
+        authenticationManagerBuilder.userDetailsService(customUserDetailsServiceImp)
+                .passwordEncoder(passwordEncoder);
 
-  }
+    }
 
-  @Override
-  protected void configure(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity.csrf().disable().sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-        .exceptionHandling().authenticationEntryPoint(entryPointAuthenticationRest).and()
-        .authorizeRequests().anyRequest().authenticated().and()
-        .addFilterBefore(jwtAuthenticationTokenFilter(), BasicAuthenticationFilter.class)
-        .formLogin().loginPage(EnumEntryPoint.LOGIN_ROUTE.getRoute())
-        .successHandler(successfulAuthenticationHandler)
-        .failureHandler(failedAuthenticationHandler).and().logout()
-        .logoutRequestMatcher(new AntPathRequestMatcher(EnumEntryPoint.TURN_OFF_ROUTE.getRoute()))
-        .logoutSuccessHandler(logoutSuccessful).deleteCookies(configProperties.getCookie());
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors().and().csrf().disable().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .exceptionHandling().authenticationEntryPoint(entryPointAuthenticationRest).and()
+                .authorizeRequests().anyRequest().authenticated().and()
+                .addFilterBefore(jwtAuthenticationTokenFilter(), BasicAuthenticationFilter.class)
+                .formLogin().loginPage(EnumEntryPoint.LOGIN_ROUTE.getRoute())
+                .successHandler(successfulAuthenticationHandler)
+                .failureHandler(failedAuthenticationHandler).and().logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher(EnumEntryPoint.TURN_OFF_ROUTE.getRoute()))
+                .logoutSuccessHandler(logoutSuccessful).deleteCookies(configProperties.getCookie());
 
-  }
+    }
+
+
+    // To enable CORS
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of("*"));  //set access from all domains
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 
 }
