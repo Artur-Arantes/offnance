400d7e9d8a7b12879b91bd9ca8489792fa34976e
Artur Arantes T <artur.a.tiraboschi@gmail.com>
2022-01-16 19:48:07 -0300

    feat(#1x07d7b): adding tagtext

--- src/main/java/br/com/artur/offnance/domain/Tag.java
+++ src/main/java/br/com/artur/offnance/domain/Tag.java
@@ -2,6 +2,19 @@ package br.com.artur.offnance.domain;
 
 import br.com.artur.offnance.domain.dto.TagOutPutDto;
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -9,21 +22,6 @@ import lombok.Generated;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.hibernate.annotations.LazyCollection;
-import org.hibernate.annotations.LazyCollectionOption;
-
-import javax.persistence.AttributeOverride;
-import javax.persistence.AttributeOverrides;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @EqualsAndHashCode(of = "id")
@@ -32,10 +30,10 @@ import java.util.List;
 @ToString(callSuper = true, includeFieldNames = true)
 @Table(name = "tags")
 @AttributeOverrides(value = {
-        @AttributeOverride(name = "id", column = @Column(name = "id_tag")),
-        @AttributeOverride(name = "version", column = @Column(name = "ver_tag")),
-        @AttributeOverride(name = "createdAt", column = @Column(name = "cre_at_tag")),
-        @AttributeOverride(name = "updatedAt", column = @Column(name = "upd_at_tag"))
+    @AttributeOverride(name = "id", column = @Column(name = "id_tag")),
+    @AttributeOverride(name = "version", column = @Column(name = "ver_tag")),
+    @AttributeOverride(name = "createdAt", column = @Column(name = "cre_at_tag")),
+    @AttributeOverride(name = "updatedAt", column = @Column(name = "upd_at_tag"))
 })
 @Generated
 @NoArgsConstructor
@@ -43,55 +41,61 @@ import java.util.List;
 public class Tag extends BaseEntity {
 
 
-    @ManyToOne
-    @JoinColumn(name = "id_usu")
-    @JsonBackReference
-    @ToString.Exclude
-    private User user;
+  @ManyToOne
+  @JoinColumn(name = "id_usu")
+  @JsonBackReference
+  @ToString.Exclude
+  private User user;
 
-    @JoinColumn(name = "id_pes")
-    @ManyToOne
-    @JsonBackReference
-    @ToString.Exclude
-    private Person person;
+  @JoinColumn(name = "id_pes")
+  @ManyToOne
+  @JsonBackReference
+  @ToString.Exclude
+  private Person person;
 
-    @Column(name = "nom_tag")
-    private String name;
+  @Column(name = "nom_tag")
+  private String name;
 
-    @ManyToOne
-    @JoinColumn(name = "id_tip")
-    @JsonBackReference
-    @ToString.Exclude
-    private Type type;
+  @ManyToOne
+  @JoinColumn(name = "id_tip")
+  @JsonBackReference
+  @ToString.Exclude
+  private Type type;
 
-    @Column(name = "per_tag")
-    private BigDecimal percentage;
+  @Column(name = "per_tag")
+  private BigDecimal percentage;
 
-    @ManyToMany
-    @JoinTable(name = "tags_dados", joinColumns = @JoinColumn(name = "id_tag", referencedColumnName = "id_tag"),
-            inverseJoinColumns = @JoinColumn(name = "id_dad", referencedColumnName = "id_dad"))
-    @JsonBackReference
-    @ToString.Exclude
-    List<Data> datas;
+  @ManyToMany
+  @JoinTable(name = "tags_dados", joinColumns = @JoinColumn(name = "id_tag", referencedColumnName = "id_tag"),
+      inverseJoinColumns = @JoinColumn(name = "id_dad", referencedColumnName = "id_dad"))
+  @JsonBackReference
+  @ToString.Exclude
+  List<Data> datas;
+
+  @ManyToMany(fetch = FetchType.LAZY)
+  @ToString.Exclude
+  @JoinTable(name = "tag_texto", joinColumns = @JoinColumn(name = "id_tag", referencedColumnName = "id_tag"),
+      inverseJoinColumns = @JoinColumn(name = "id_tex", referencedColumnName = "id_tex"))
+  private List<Text> texts;
 
 
-    public List<Data> getDatas() {
-        if (datas == null) {
-            datas = new ArrayList<>();
-        }
-        return datas;
+  public List<Data> getDatas() {
+    if (datas == null) {
+      datas = new ArrayList<>();
     }
+    return datas;
+  }
 
-    public TagOutPutDto toOutPutDto() {
+  public TagOutPutDto toOutPutDto() {
 
-        final var tagOutPutDto = TagOutPutDto.builder()
-                .name(name)
-                .user(TagOutPutDto.UserOutPutDto.builder().username(user.getUsername()).build())
-                .person(TagOutPutDto.PersonOutPutDto.builder().name(person.getName()).build())
-                .type(TagOutPutDto.TypeOutputDto.builder().name(type.getName()).build())
-                .id(getId())
-                .percentage(getPercentage())
-                .build();
-        return tagOutPutDto;
-    }
+    final var tagOutPutDto = TagOutPutDto.builder()
+        .name(name)
+        .user(TagOutPutDto.UserOutPutDto.builder().username(user.getUsername()).build())
+        .person(TagOutPutDto.PersonOutPutDto.builder().name(person.getName()).build())
+        .type(TagOutPutDto.TypeOutputDto.builder().name(type.getName()).build())
+        .id(getId())
+        .percentage(getPercentage())
+        .build();
+    return tagOutPutDto;
+  }
 }
---
+++ src/main/java/br/com/artur/offnance/repositories/TextRepository.java
@@ -0,0 +1,8 @@
+package br.com.artur.offnance.repositories;
+
+import br.com.artur.offnance.domain.Text;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TextRepository extends CrudRepository<Text, Long> {
+
+}
